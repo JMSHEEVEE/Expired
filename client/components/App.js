2e162ccb6f7f5f43e9a7727f8b71f1f416f95123
shirley-luu <sh.rleyluu@gmail.com>
2022-10-17 18:52:27 -0400

    boilerplate

---
+++ client/components/App.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "../scss/styles.scss";
+
+const App = () => (
+  <div className="logo">
+    <img src="https://hmp.me/dym2"></img>
+  </div>
+);
+
+export default App;
---
+++ server/controllers/controller.js
@@ -0,0 +1,5 @@
+const models = require("../models/model");
+
+const controllers = {};
+
+module.exports = controllers;
