ebf59a1b510933b0f00369eb00e9c29e8448dd4c
shirley-luu <sh.rleyluu@gmail.com>
2022-10-18 19:38:20 -0400

    fully working new uri

--- server/models/model.js
+++ server/models/model.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 // please enter the postgres URI here! thank you!!
-const PG_URI = "postgres://vdvvejux:CS4RinjnJ6z-L-MZMFI6lEAlMhq2pEtm@peanut.db.elephantsql.com/vdvvejux";
+const PG_URI = "postgres://sejnvhxh:TXPhGUk3h9w5TEplugO1l8FWAwnajZXb@peanut.db.elephantsql.com/sejnvhxh";
 
 const pool = new Pool({
   connectionString: PG_URI,
